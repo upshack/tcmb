222a493ad6473a1996c0db4ec29604ebb681d3e7
Onur Alemdar <53017093+upshack@users.noreply.github.com>
2024-12-06 22:20:29 +0000

    tst

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,11 @@ import plotly.graph_objects as go
 API_KEY = st.secrets['API_KEY']
 evds = evdsAPI(API_KEY)
 
-data = evds.get_data(['TP.DK.USD.A.YTL','TP.DK.USD.S.YTL'], 
+# Veri çekme
+data = evds.get_data(['TP.DK.USD.A.YTL', 'TP.DK.USD.S.YTL'], 
                      startdate="01-05-2024", enddate="30-11-2024")
-print(data)
 
-
+# DataFrame'e dönüştürme
 df = pd.DataFrame(data)
 
 # Tarih sütununu datetime formatına dönüştürme
@@ -23,14 +23,14 @@ df['Tarih'] = pd.to_datetime(df['Tarih'], format='%d-%m-%Y')
 df['TP_DK_USD_A_YTL'] = df['TP_DK_USD_A_YTL'].interpolate()
 df['TP_DK_USD_S_YTL'] = df['TP_DK_USD_S_YTL'].interpolate()
 
-# Plotly grafik oluşturma (Sadece çizgiler)
+# Plotly grafik oluşturma
 fig = go.Figure()
 
 # Dolar alış kuru grafiği
 fig.add_trace(go.Scatter(
     x=df['Tarih'],
     y=df['TP_DK_USD_A_YTL'],
-    mode='lines',  # Noktalar yerine sadece çizgiler
+    mode='lines',  # Sadece çizgiler
     name='Dolar Alış Kuru (YTL)',
     line=dict(color='blue')
 ))
@@ -39,24 +39,28 @@ fig.add_trace(go.Scatter(
 fig.add_trace(go.Scatter(
     x=df['Tarih'],
     y=df['TP_DK_USD_S_YTL'],
-    mode='lines',  # Noktalar yerine sadece çizgiler
+    mode='lines',  # Sadece çizgiler
     name='Dolar Satış Kuru (YTL)',
     line=dict(color='red')
 ))
 
-# Grafik başlık ve etiketler
+# Grafik başlık ve düzenleme
 fig.update_layout(
     title='Dolar Alış ve Satış Kuru Zaman Serisi (Mayıs-Kasım 2024)',
     xaxis_title='Tarih',
     yaxis_title='Kuruş (YTL)',
     legend_title="Kurlar",
-    template='plotly_dark',  # Temayı değiştirebilirsiniz
+    template='plotly_dark',
     xaxis=dict(
-        tickformat='%d-%m-%Y',  # Tarih formatı
-        tickangle=45  # X ekseni etiketlerini döndürme
+        tickformat='%d-%m-%Y',
+        tickangle=45
     ),
     margin=dict(l=40, r=40, t=40, b=40)
 )
 
-# Grafiği gösterme
-fig.show()
+# Streamlit başlık ve açıklama
+st.title("Dolar Kuru Zaman Serisi Grafiği")
+st.write("Aşağıdaki grafik Mayıs-Kasım 2024 tarihleri arasındaki dolar alış ve satış kurlarını göstermektedir.")
+
+# Streamlit'te Plotly grafiği gösterme
+st.plotly_chart(fig, use_container_width=True)
